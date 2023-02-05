ba0f5fed20cc0fe999f0922d0c9ddd52b3fce166
邵😉😉 <736083950@qq.com>
2023-02-05 21:59:21 +0800

    启动项目

--- vue3_admin/src/main.js
+++ vue3_admin/src/main.js
@@ -1,11 +1,14 @@
 import {
     createApp
 } from 'vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 import App from './App.vue'
 import router from '@/router'
 
 const app = createApp(App) // 生成 Vue 实例 app
 
 app.use(router) // 引用路由实例
+app.use(ElementPlus)
 
 app.mount('#app') // 挂载到 #app
--- vue3_admin/src/views/Index.vue
+++ vue3_admin/src/views/Index.vue
@@ -1,5 +1,5 @@
 <template>
-  Index
+  <el-button type="primary">主要按钮</el-button>
 </template>
 
 <script>
