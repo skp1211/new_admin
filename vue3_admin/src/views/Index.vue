bf184356a06cb56ef289220fca444d5c88b4371b
Nick <286096598@qq.com>
2023-02-06 18:09:44 +0800

    begin

--- vue3_admin/src/views/Index.vue
+++ vue3_admin/src/views/Index.vue
@@ -1,10 +1,12 @@
 <template>
   <el-button type="primary">主要按钮</el-button>
+  <el-button :plain="true" @click="open4">error</el-button>
 </template>
 
-<script>
-export default {};
+<script setup>
+const open4 = () => {
+  ElMessage.error("Oops, this is a error message.");
+};
 </script>
-
 <style>
 </style>
--- vue3_admin/vite.config.js
+++ vue3_admin/vite.config.js
@@ -8,12 +8,16 @@ import {
 } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import {
+  ElementPlusResolver
+} from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default ({
   mode
 }) => defineConfig({
-  plugins: [vue()],
   resolve: {
     alias: {
       // '@': fileURLToPath(new URL('./src', import.meta.url))
@@ -31,5 +35,26 @@ export default ({
         rewrite: path => path.replace(/^\/api/, '') // 重写 api 为 空，就是去掉它
       }
     }
-  }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@use "@/styles/element/index.scss" as *;`,
+      },
+    },
+  },
+  plugins: [
+    vue(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver({
+        importStyle: 'sass'
+      })],
+    }),
+  ]
 })
