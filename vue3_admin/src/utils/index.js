6e34f2bc2d5f7c6038d57efdc96996e7a3c986cb
Nick <286096598@qq.com>
2023-02-07 10:17:10 +0800

    基础搭建

---
+++ vue3_admin/src/utils/index.js
@@ -0,0 +1,17 @@
+// 公用方法封装
+export function localGet(key) {
+    const value = window.localStorage.getItem(key)
+    try {
+        return JSON.parse(window.localStorage.getItem(key))
+    } catch (error) {
+        return value
+    }
+}
+
+export function localSet(key, value) {
+    window.localStorage.setItem(key, JSON.stringify(value))
+}
+
+export function localRemove(key) {
+    window.localStorage.removeItem(key)
+}
